59ed95673e6b4931ac540ae6f82af2a9942eec09
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-06-14 14:22:27 +0200

    fix(hack.py): Updates path to models since wrong prediction models were loaded

--- src/ralf/hack.py
+++ src/ralf/hack.py
@@ -5,6 +5,6 @@ import sys
 
 if __name__ == "__main__":
     facing_robot, bottom_face, object_type, mln_path = sys.argv[1:]
-    set_mln_path(mln_path)
+    set_mln_path(mln_path+'/mln')
     inference = Inference(facing_robot, bottom_face, object_type)
     print inference.get_storted_list_of_grasping_types_based_on_probability()
